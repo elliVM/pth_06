0f766eafea621e387f037bb4727b42a56535d12f
elliVM <126466762+elliVM@users.noreply.github.com>
2025-09-24 11:11:20 +0000

    Deploying to gh-pages from @ elliVM/pth_06@bb056f60270ca07ef9dd93d0069a817777f037bd 🚀

--- docs/doxygen/doxygen/search/classes_1.js
+++ docs/doxygen/doxygen/search/classes_1.js
@@ -8,7 +8,5 @@ var searchData=
   ['bloomdb_5',['Bloomdb',['../classcom_1_1teragrep_1_1pth__06_1_1jooq_1_1generated_1_1bloomdb_1_1Bloomdb.html',1,'com::teragrep::pth_06::jooq::generated::bloomdb']]],
   ['bloomquerycondition_6',['BloomQueryCondition',['../interfacecom_1_1teragrep_1_1pth__06_1_1planner_1_1walker_1_1conditions_1_1BloomQueryCondition.html',1,'com::teragrep::pth_06::planner::walker::conditions']]],
   ['bucket_7',['Bucket',['../classcom_1_1teragrep_1_1pth__06_1_1jooq_1_1generated_1_1journaldb_1_1tables_1_1Bucket.html',1,'com::teragrep::pth_06::jooq::generated::journaldb::tables']]],
-  ['bucketrecord_8',['BucketRecord',['../classcom_1_1teragrep_1_1pth__06_1_1jooq_1_1generated_1_1journaldb_1_1tables_1_1records_1_1BucketRecord.html',1,'com::teragrep::pth_06::jooq::generated::journaldb::tables::records']]],
-  ['bytespersecondmetricaggregator_9',['BytesPerSecondMetricAggregator',['../classcom_1_1teragrep_1_1pth__06_1_1metrics_1_1bytes_1_1BytesPerSecondMetricAggregator.html',1,'com::teragrep::pth_06::metrics::bytes']]],
-  ['bytesprocessedmetricaggregator_10',['BytesProcessedMetricAggregator',['../classcom_1_1teragrep_1_1pth__06_1_1metrics_1_1bytes_1_1BytesProcessedMetricAggregator.html',1,'com::teragrep::pth_06::metrics::bytes']]]
+  ['bucketrecord_8',['BucketRecord',['../classcom_1_1teragrep_1_1pth__06_1_1jooq_1_1generated_1_1journaldb_1_1tables_1_1records_1_1BucketRecord.html',1,'com::teragrep::pth_06::jooq::generated::journaldb::tables::records']]]
 ];
