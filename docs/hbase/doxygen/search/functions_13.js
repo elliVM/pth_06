0ce9c0e629487c708831dc39dd1be2659de59380
elliVM <126466762+elliVM@users.noreply.github.com>
2025-11-19 08:57:44 +0000

    Deploying to gh-pages from @ elliVM/pth_06@e297cdc85050c6a7a63c865a97d7504539f31e77 🚀

--- docs/hbase/doxygen/search/functions_13.js
+++ docs/hbase/doxygen/search/functions_13.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['uniquechildren_0',['UniqueChildren',['../classcom_1_1teragrep_1_1pth__06_1_1ast_1_1transform_1_1UniqueChildren.html#a216e1ddf2de5a6ffc2124194f4996ff8',1,'com::teragrep::pth_06::ast::transform::UniqueChildren']]],
-  ['unixepochfromstring_1',['unixEpochFromString',['../classcom_1_1teragrep_1_1pth__06_1_1planner_1_1walker_1_1Util.html#a5f294d9d9617f9681f05b9154c40d409',1,'com::teragrep::pth_06::planner::walker::Util']]]
+  ['unixepochfromstring_1',['unixEpochFromString',['../classcom_1_1teragrep_1_1pth__06_1_1planner_1_1walker_1_1Util.html#a5f294d9d9617f9681f05b9154c40d409',1,'com::teragrep::pth_06::planner::walker::Util']]],
+  ['usearchive_2',['usearchive',['../classcom_1_1teragrep_1_1pth__06_1_1ArchiveMicroStreamReader.html#a755d5e92276f9154edcea0fec4f59dec',1,'com.teragrep.pth_06.ArchiveMicroStreamReader.useArchive()'],['../classcom_1_1teragrep_1_1pth__06_1_1scheduler_1_1Batch.html#aa8902123db6ab2966552653c3bbe79c6',1,'com.teragrep.pth_06.scheduler.Batch.useArchive()']]],
+  ['usehbase_3',['usehbase',['../classcom_1_1teragrep_1_1pth__06_1_1ArchiveMicroStreamReader.html#a00eed7fba1d800a7745cccf4d729836d',1,'com.teragrep.pth_06.ArchiveMicroStreamReader.useHBase()'],['../classcom_1_1teragrep_1_1pth__06_1_1scheduler_1_1Batch.html#ae5c9ab1db6f0c2979fd0bd6e55fcce8c',1,'com.teragrep.pth_06.scheduler.Batch.useHBase()']]],
+  ['usekafka_4',['usekafka',['../classcom_1_1teragrep_1_1pth__06_1_1ArchiveMicroStreamReader.html#a2ae6ecb8b1cd94d98665cb042886cb0a',1,'com.teragrep.pth_06.ArchiveMicroStreamReader.useKafka()'],['../classcom_1_1teragrep_1_1pth__06_1_1scheduler_1_1Batch.html#abb1dffc532f4dae1b5d88ed768bd2f23',1,'com.teragrep.pth_06.scheduler.Batch.useKafka()']]]
 ];
