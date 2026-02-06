2d6638247863dd97034e9557528ec15f8c635928
elliVM <126466762+elliVM@users.noreply.github.com>
2026-02-06 08:35:57 +0000

    Deploying to gh-pages from @ elliVM/pth_06@4ab26bb460537146f371e0bfe1f2b27434e75c98 🚀

--- docs/exclude-pattern-mode/doxygen/search/files_14.js
+++ docs/exclude-pattern-mode/doxygen/search/files_14.js
@@ -2,7 +2,5 @@ var searchData=
 [
   ['weightedoffset_2ejava_0',['WeightedOffset.java',['../WeightedOffset_8java.html',1,'']]],
   ['withoutfilterscondition_2ejava_1',['WithoutFiltersCondition.java',['../WithoutFiltersCondition_8java.html',1,'']]],
-  ['withoutfiltersconditiontest_2ejava_2',['WithoutFiltersConditionTest.java',['../WithoutFiltersConditionTest_8java.html',1,'']]],
-  ['withoutpatterncondition_2ejava_3',['WithoutPatternCondition.java',['../WithoutPatternCondition_8java.html',1,'']]],
-  ['withoutpatternconditiontest_2ejava_4',['WithoutPatternConditionTest.java',['../WithoutPatternConditionTest_8java.html',1,'']]]
+  ['withoutfiltersconditiontest_2ejava_2',['WithoutFiltersConditionTest.java',['../WithoutFiltersConditionTest_8java.html',1,'']]]
 ];
